3ff91dc03aae90b08364137d1dbdada6c0f7d9e2
Koyejo <adinlewakoyejo@yahoo.com>
2023-11-01 19:31:37 +0000

    Made SQL style guide corrections

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
 -- Create a table named 'users', with an enumerated column
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+    PRIMARY KEY (id),
+    id      INT                    NOT NULL AUTO_INCREMENT,
+    email   VARCHAR(255)           NOT NULL UNIQUE,
+    name    VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL
 );
