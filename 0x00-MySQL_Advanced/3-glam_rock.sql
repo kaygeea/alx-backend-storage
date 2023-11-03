b1597a21c6e7c423e4da42af25bb67a5cb1245c6
Koyejo <adinlewakoyejo@yahoo.com>
2023-11-03 09:35:57 +0000

    Add script to select bands with 'Glam rock' style

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,10 @@
+-- Rank bands, with 'Glam Rock' style, based on their longeity
+-- Longevity is based on `split - formed`
+SELECT band_name, 
+       CASE
+           WHEN split IS NULL THEN '2020'
+           ELSE split
+       END - formed AS lifespan
+  FROM metal_bands
+ WHERE IFNULL(style, '') LIKE '%Glam rock%'
+ ORDER BY lifespan DESC;
