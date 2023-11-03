13ac4594ea20ffd421f0064f3715a5fffab7f0c9
Koyejo <adinlewakoyejo@yahoo.com>
2023-11-03 15:10:12 +0000

    Correct column name

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Rank country origins of bands
 -- Ordered by the number of (non-unique) fans
-SELECT origin, SUM(nb_fans) AS total_fans
+SELECT origin, SUM(nb_fans) AS nb_fans
   FROM metal_bands
  GROUP BY origin
- ORDER BY total_fans DESC;
+ ORDER BY nb_fans DESC;
