99b1e208534295e7d6e1dfdc9874542edd6ca71f
Koyejo <adinlewakoyejo@yahoo.com>
2023-11-03 14:50:22 +0000

    Add trigger to update field before update on table

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- Create trigger that resets 'valid_email' when 'email' has been changed
+DELIMITER $$
+
+CREATE TRIGGER resetValidEmail
+ BEFORE UPDATE
+            ON users
+  FOR EACH ROW
+         BEGIN
+	         IF NEW.email != OLD.email
+	       THEN
+                    SET NEW.valid_email = 0;
+	        END IF;
+         END $$
+
+DELIMITER ;
