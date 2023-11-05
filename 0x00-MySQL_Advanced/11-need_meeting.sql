3ca2957b5de7e71a61b37fa15a71216c3b47e250
Koyejo <adinlewakoyejo@yahoo.com>
2023-11-05 01:03:07 +0000

    Change comparison operator to '<' for date of last_meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,7 +2,7 @@
 CREATE VIEW need_meeting AS
      SELECT name
        FROM students
-      WHERE score < 80.0 AND
+      WHERE score < 80.0 AND -- 80.0 for strict qualifier
             (last_meeting IS NULL
-             OR last_meeting > DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
---           OR DATEDIFF(CURDATE(), last_meeting) > 31);
+             OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
+--             OR DATEDIFF(CURDATE(), last_meeting) > 31);
